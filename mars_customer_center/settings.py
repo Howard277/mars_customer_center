8844b5194ff385bc679d0b2e86751f4db1b73134
wuketao <wuketao@houbank.com>
2019-01-10 18:44:34 +0800

    优化

--- mars_customer_center/settings.py
+++ mars_customer_center/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'ny&@m!b=izgkn--61gkbtq_ychi3k%gn0e=l$1378im^mb2u(%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
